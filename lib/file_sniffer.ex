cd93b4269b36d897352ee451663ca5f508b144de
junaidnaeem <jq601137@gmail.com>
2023-02-21 11:28:57 -0600

    File Sniffer

---
+++ lib/file_sniffer.ex
@@ -0,0 +1,49 @@
+defmodule FileSniffer do
+  def type_from_extension(extension) do
+   case extension do
+    "exe" -> "application/octet-stream"
+    "bmp" -> "image/bmp"
+    "png" -> "image/png"
+    "jpg" -> "image/jpg"
+    "gif" -> "image/gif"
+      _   ->  nil
+    end    
+  end
+
+  def type_from_binary(file_binary) do
+       case file_binary do
+    <<0x7F, 0x45, 0x4C, 0x46, _::binary>> -> "application/octet-stream"
+    <<0x42, 0x4D, _::binary>> -> "image/bmp"
+    <<0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A, _::binary>> -> "image/png"
+    <<0xFF, 0xD8, 0xFF, _::binary>> -> "image/jpg"
+    <<0x47, 0x49, 0x46, _::binary>> -> "image/gif"
+      _   ->  nil
+    end  
+    
+  end
+
+  def verify(file_binary, extension) do
+   value = type_from_binary(file_binary)
+      check(value, extension) 
+  end  
+    def check(value, ext) when value == "application/octet-stream" and ext=="exe" do
+      {:ok, value}
+    end
+    def check(value, ext) when value == "image/bmp" and ext=="bmp" do
+      {:ok, value}
+    end
+    def check(value, ext) when value == "image/png" and ext=="png" do
+      {:ok, value}
+    end
+    def check(value, ext) when value == "image/jpg" and ext=="jpg" do
+      {:ok, value}
+    end
+    def check(value, ext) when value == "image/gif" and ext=="gif" do
+      {:ok, value}
+    end
+    def check(value, ext) do
+      {:error, "Warning, file format and file extension do not match."}
+    end
+    
+  
+end
